bac6a3b2ff2bc0b61d3b9f538b94c6aea98edfd2
E66666666 <emily66666666ok@outlook.com>
2020-09-20 19:40:48 +0200

    Add subhandlingdata base stuff

--- RTHandlingEditor/Memory/HandlingInfo.h
+++ RTHandlingEditor/Memory/HandlingInfo.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "atArray.h"
 #include <cstdint>
 
 namespace RTHE {
@@ -8,24 +9,33 @@ namespace RTHE {
         float z;
     };
 
+    class CBaseSubHandlingData
+    {
+    public:
+        virtual ~CBaseSubHandlingData() = default;
+        virtual void* GetParser() = 0;
+        virtual int GetUnk() = 0;
+        virtual void ProcessOnLoad() = 0;
+    };
+
     class CHandlingData {
     public:
-        virtual void Function0();               //
-        char pad_0x0008[0x4];                   //0x0008
+        virtual ~CHandlingData() = default;
+        uint32_t NameHash;                      //0x0008
         float fMass;                            //0x000C
         float fInitialDragCoeff;                //0x0010
         float fDownforceModifier;               //0x0014
-        float N000019C0;                        //0x0018
+        float fPopUpLightRotation;              //0x0018
         float N000019E5;                        //0x001C
         Vector3 vecCentreOfMassOffset;          //0x0020
         float N000019E9;                        //0x002C
         Vector3 vecInertiaMultiplier;           //0x0030
-        char pad_0x003C[0x4];                   //0x003C
+        float fUnk_0x003C;                      //0x003C // vecInertiaMultiplier.x?
         float fPercentSubmerged;                //0x0040
         float fSubmergedRatio_;                 //0x0044
         float fDriveBiasFront;                  //0x0048
         float fDriveBiasRear;                   //0x004C // fDriveBiasRear
-        __int32 nInitialDriveGears;             //0x0050
+        int nInitialDriveGears;                 //0x0050
         float fDriveInertia;                    //0x0054
         float fClutchChangeRateScaleUpShift;    //0x0058
         float fClutchChangeRateScaleDownShift;  //0x005C
@@ -74,13 +84,35 @@ namespace RTHE {
         float N00001A75;                        //0x0108
         Vector3 vecSeatOffsetDist;              //0x010C
         int nMonetaryValue;                     //0x0118
-        char pad_0x11C[0x08];                   //0x011C
+        float fUnk_0x11C;                       //0x011C // 1.25f ?
+        float fUnk_0x120;                       //0x0120 // 70.0f ?
         uint32_t strModelFlags;                 //0x0124
         uint32_t strHandlingFlags;              //0x0128
         uint32_t strDamageFlags;                //0x012C
-        char pad_0x130[0x0C];                   //0x0130
+        char pad_0x130[0x08];                   //0x0130
+        uint32_t UnkHash0;                      //0x0138 // Seems to be joaat(0)
         uint32_t AIHandling;                    //0x013C
+        char pad_0x140[0x18];                   //0x0140
+        atArray<CBaseSubHandlingData*> m_subHandlingData;//0x0158
     }; //Size=0x01C0 ?
     static_assert(offsetof(CHandlingData, strModelFlags) == 0x124, "wrong offset");
     static_assert(offsetof(CHandlingData, AIHandling)    == 0x13C, "wrong offset");
+    static_assert(offsetof(CHandlingData, m_subHandlingData) == 0x158, "wrong offset");
+
+    // CCarHandlingData: (@0?)
+    // vtable? (ptr)                           //0x0000
+    // fBackEndPopUpCarImpulseMult             //0x0008
+    // fBackEndPopUpBuildingImpulseMult        //0x000C
+    // fBackEndPopUpMaxDeltaSpeed              //0x0010
+    // fToeFront                               //0x0014
+    // fToeRear                                //0x0018
+    // fCamberFront                            //0x001C
+    // fCamberRear                             //0x0020
+    // fCastor                                 //0x0024
+    // fEngineResistance                       //0x0028
+    // ? -1.0                                  //0x002C
+    // ?  1.0                                  //0x0030
+    // ?  1.0                                  //0x0034
+    // ? 0x9E58B8A9                            //0x0038
+    // strAdvancedFlags                        //0x003C
 }
