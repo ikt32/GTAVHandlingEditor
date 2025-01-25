a7c424a3146e801f6e7b7cd10038682ae567ecc7
ikt <6639510+ikt32@users.noreply.github.com>
2025-01-25 11:35:21 +0100

    Add resource file indicating compatibility in FiveM

--- RTHandlingEditor/Constants.h
+++ RTHandlingEditor/Constants.h
@@ -4,8 +4,8 @@
 
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 0
-#define VERSION_PATCH 0
-//#define VERSION_LABEL ""
+#define VERSION_PATCH 1
+// #define VERSION_LABEL "test"
 
 #ifdef VERSION_LABEL
 #define VERSION_LABEL_TXT "-" VERSION_LABEL
@@ -13,9 +13,10 @@
 #define VERSION_LABEL_TXT ""
 #endif
 
+#define VERSION_DISPLAY STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH) VERSION_LABEL_TXT
+
 namespace Constants {
-    static const char* const DisplayVersion =
-        "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH) VERSION_LABEL_TXT;
+    static const char* const DisplayVersion = VERSION_DISPLAY;
 
     static const char* const iktDir = "ikt";
     static const char* const ModDir = "HandlingEditor";
--- RTHandlingEditor/Memory/Versions.h
+++ RTHandlingEditor/Memory/Versions.h
@@ -4,136 +4,146 @@
 #include "../Util/FileVersion.h"
 
 static std::vector<std::string> GameVersionString = {
-    "VER_1_0_335_2_STEAM",      // 00
-    "VER_1_0_335_2_NOSTEAM",    // 01
+    "v1.0.335.2.STEAM",      // 00
+    "v1.0.335.2.NOSTEAM",    // 01
 
-    "VER_1_0_350_1_STEAM",      // 02
-    "VER_1_0_350_2_NOSTEAM",    // 03
+    "v1.0.350.1.STEAM",      // 02
+    "v1.0.350.2.NOSTEAM",    // 03
 
-    "VER_1_0_372_2_STEAM",      // 04
-    "VER_1_0_372_2_NOSTEAM",    // 05
+    "v1.0.372.2.STEAM",      // 04
+    "v1.0.372.2.NOSTEAM",    // 05
 
-    "VER_1_0_393_2_STEAM",      // 06
-    "VER_1_0_393_2_NOSTEAM",    // 07
+    "v1.0.393.2.STEAM",      // 06
+    "v1.0.393.2.NOSTEAM",    // 07
 
-    "VER_1_0_393_4_STEAM",      // 08
-    "VER_1_0_393_4_NOSTEAM",    // 09
+    "v1.0.393.4.STEAM",      // 08
+    "v1.0.393.4.NOSTEAM",    // 09
 
-    "VER_1_0_463_1_STEAM",      // 10
-    "VER_1_0_463_1_NOSTEAM",    // 11
+    "v1.0.463.1.STEAM",      // 10
+    "v1.0.463.1.NOSTEAM",    // 11
 
-    "VER_1_0_505_2_STEAM",      // 12
-    "VER_1_0_505_2_NOSTEAM",    // 13
+    "v1.0.505.2.STEAM",      // 12
+    "v1.0.505.2.NOSTEAM",    // 13
 
-    "VER_1_0_573_1_STEAM",      // 14
-    "VER_1_0_573_1_NOSTEAM",    // 15
+    "v1.0.573.1.STEAM",      // 14
+    "v1.0.573.1.NOSTEAM",    // 15
 
-    "VER_1_0_617_1_STEAM",      // 16
-    "VER_1_0_617_1_NOSTEAM",    // 17
+    "v1.0.617.1.STEAM",      // 16
+    "v1.0.617.1.NOSTEAM",    // 17
 
-    "VER_1_0_678_1_STEAM",      // 18
-    "VER_1_0_678_1_NOSTEAM",    // 19
+    "v1.0.678.1.STEAM",      // 18
+    "v1.0.678.1.NOSTEAM",    // 19
 
-    "VER_1_0_757_2_STEAM",      // 20
-    "VER_1_0_757_2_NOSTEAM",    // 21
+    "v1.0.757.2.STEAM",      // 20
+    "v1.0.757.2.NOSTEAM",    // 21
 
-    "VER_1_0_757_4_STEAM",      // 22
-    "VER_1_0_757_4_NOSTEAM",    // 23
+    "v1.0.757.4.STEAM",      // 22
+    "v1.0.757.4.NOSTEAM",    // 23
 
-    "VER_1_0_791_2_STEAM",      // 24
-    "VER_1_0_791_2_NOSTEAM",    // 25
+    "v1.0.791.2.STEAM",      // 24
+    "v1.0.791.2.NOSTEAM",    // 25
 
-    "VER_1_0_877_1_STEAM",      // 26
-    "VER_1_0_877_1_NOSTEAM",    // 27
+    "v1.0.877.1.STEAM",      // 26
+    "v1.0.877.1.NOSTEAM",    // 27
 
-    "VER_1_0_944_2_STEAM",      // 28
-    "VER_1_0_944_2_NOSTEAM",    // 29
+    "v1.0.944.2.STEAM",      // 28
+    "v1.0.944.2.NOSTEAM",    // 29
 
-    "VER_1_0_1011_1_STEAM",     // 30
-    "VER_1_0_1011_1_NOSTEAM",   // 31
+    "v1.0.1011.1.STEAM",     // 30
+    "v1.0.1011.1.NOSTEAM",   // 31
 
-    "VER_1_0_1032_1_STEAM",     // 32
-    "VER_1_0_1032_1_NOSTEAM",   // 33
+    "v1.0.1032.1.STEAM",     // 32
+    "v1.0.1032.1.NOSTEAM",   // 33
 
-    "VER_1_0_1103_2_STEAM",     // 34
-    "VER_1_0_1103_2_NOSTEAM",   // 35
+    "v1.0.1103.2.STEAM",     // 34
+    "v1.0.1103.2.NOSTEAM",   // 35
 
-    "VER_1_0_1180_2_STEAM",     // 36
-    "VER_1_0_1180_2_NOSTEAM",   // 37
+    "v1.0.1180.2.STEAM",     // 36
+    "v1.0.1180.2.NOSTEAM",   // 37
 
-    "VER_1_0_1290_1_STEAM",     // 38
-    "VER_1_0_1290_1_NOSTEAM",   // 39
+    "v1.0.1290.1.STEAM",     // 38
+    "v1.0.1290.1.NOSTEAM",   // 39
 
-    "VER_1_0_1365_1_STEAM",     // 40
-    "VER_1_0_1365_1_NOSTEAM",   // 41
+    "v1.0.1365.1.STEAM",     // 40
+    "v1.0.1365.1.NOSTEAM",   // 41
 
-    "VER_1_0_1493_0_STEAM",     // 42
-    "VER_1_0_1493_0_NOSTEAM",   // 43
+    "v1.0.1493.0.STEAM",     // 42
+    "v1.0.1493.0.NOSTEAM",   // 43
 
-    "VER_1_0_1493_1_STEAM",     // 44
-    "VER_1_0_1493_1_NOSTEAM",   // 45
+    "v1.0.1493.1.STEAM",     // 44
+    "v1.0.1493.1.NOSTEAM",   // 45
 
-    "VER_1_0_1604_0_STEAM",     // 46
-    "VER_1_0_1604_0_NOSTEAM",   // 47
+    "v1.0.1604.0.STEAM",     // 46
+    "v1.0.1604.0.NOSTEAM",   // 47
 
-    "VER_1_0_1604_1_STEAM",     // 48
-    "VER_1_0_1604_1_NOSTEAM",   // 49
+    "v1.0.1604.1.STEAM",     // 48
+    "v1.0.1604.1.NOSTEAM",   // 49
 
-    //"VER_1_0_1734_0_STEAM",   // XX
-    //"VER_1_0_1734_0_NOSTEAM", // XX
+    //"v1.0.1734.0.STEAM",   // XX
+    //"v1.0.1734.0.NOSTEAM", // XX
 
-    "VER_1_0_1737_0_STEAM",     // 50
-    "VER_1_0_1737_0_NOSTEAM",   // 51
+    "v1.0.1737.0.STEAM",     // 50
+    "v1.0.1737.0.NOSTEAM",   // 51
 
-    "VER_1_0_1737_6_STEAM",     // 52
-    "VER_1_0_1737_6_NOSTEAM",   // 53
+    "v1.0.1737.6.STEAM",     // 52
+    "v1.0.1737.6.NOSTEAM",   // 53
 
-    "VER_1_0_1868_0_STEAM",     // 54
-    "VER_1_0_1868_0_NOSTEAM",   // 55
+    "v1.0.1868.0.STEAM",     // 54
+    "v1.0.1868.0.NOSTEAM",   // 55
 
-    "VER_1_0_1868_1_STEAM",     // 56
-    "VER_1_0_1868_1_NOSTEAM",   // 57
+    "v1.0.1868.1.STEAM",     // 56
+    "v1.0.1868.1.NOSTEAM",   // 57
 
-    "VER_1_0_1868_4_EGS",       // 58
+    "v1.0.1868.4.EGS",       // 58
 
-    "VER_1_0_2060_0_STEAM",     // 59
-    "VER_1_0_2060_0_NOSTEAM",   // 60
+    "v1.0.2060.0.STEAM",     // 59
+    "v1.0.2060.0.NOSTEAM",   // 60
 
-    "VER_1_0_2060_1_STEAM",     // 61
-    "VER_1_0_2060_1_NOSTEAM",   // 62
+    "v1.0.2060.1.STEAM",     // 61
+    "v1.0.2060.1.NOSTEAM",   // 62
 
-    "VER_1_0_2189_0_STEAM",     // 63
-    "VER_1_0_2189_0_NOSTEAM",   // 64
+    "v1.0.2189.0.STEAM",     // 63
+    "v1.0.2189.0.NOSTEAM",   // 64
 
-    "VER_1_0_2215_0_STEAM",     // 65
-    "VER_1_0_2215_0_NOSTEAM",   // 66
+    "v1.0.2215.0.STEAM",     // 65
+    "v1.0.2215.0.NOSTEAM",   // 66
 
-    "VER_1_0_2245_0_STEAM",     // 67
-    "VER_1_0_2245_0_NOSTEAM",   // 68
+    "v1.0.2245.0.STEAM",     // 67
+    "v1.0.2245.0.NOSTEAM",   // 68
 
-    "VER_1_0_2372_0_STEAM",     // 69
-    "VER_1_0_2372_0_NOSTEAM",   // 70
+    "v1.0.2372.0.STEAM",     // 69
+    "v1.0.2372.0.NOSTEAM",   // 70
 
-    "VER_1_0_2545_0_STEAM",     // 71
-    "VER_1_0_2545_0_NOSTEAM",   // 72
+    "v1.0.2545.0.STEAM",     // 71
+    "v1.0.2545.0.NOSTEAM",   // 72
 
-    "VER_1_0_2612_1_STEAM",     // 73
-    "VER_1_0_2612_1_NOSTEAM",   // 74
+    "v1.0.2612.1.STEAM",     // 73
+    "v1.0.2612.1.NOSTEAM",   // 74
 
-    "VER_1_0_2628_2_STEAM",     // 75
-    "VER_1_0_2628_2_NOSTEAM",   // 76
+    "v1.0.2628.2.STEAM",     // 75
+    "v1.0.2628.2.NOSTEAM",   // 76
 
-    "VER_1_0_2699_0_STEAM",     // 77
-    "VER_1_0_2699_0_NOSTEAM",   // 78
+    "v1.0.2699.0.STEAM",     // 77
+    "v1.0.2699.0.NOSTEAM",   // 78
 
-    "VER_1_0_2699_16",          // 79
-    "VER_1_0_2802_0",           // 80
-
-    "VER_SIZE",                 // 81
-    "VER_UNK"                   // -1
+    "v1.0.2699.16",          // 79
+    "v1.0.2802.0",           // 80
+    "v1.0.2824.0",           // 81
+    "v1.0.2845.0",           // 82
+    "v1.0.2944.0",           // 83
+    "v1.0.3028.0",           // 84
+    "v1.0.3095.0",           // 85
+    "v1.0.3179.0",           // 86
+    "v1.0.3258.0",           // 87
+    "v1.0.3274.0",           // 88
+    "v1.0.3323.0",           // 89
+    "v1.0.3337.0",           // 90
+    "v1.0.3351.0",           // 91
+    "v1.0.3407.0",           // 92
+    "v1.0.3411.0",           // 93
 };
 
-enum G_GameVersion : int {
+enum G_GameVersion: int {
     G_VER_1_0_335_2_STEAM,      // 00
     G_VER_1_0_335_2_NOSTEAM,    // 01
 
@@ -257,16 +267,130 @@ enum G_GameVersion : int {
     G_VER_1_0_2699_0_NOSTEAM,   // 78
 
     G_VER_1_0_2699_16,          // 79
-
     G_VER_1_0_2802_0,           // 80
-
-    G_VER_SIZE,                 // 81
-    G_VER_UNK = -1              // -1
+    G_VER_1_0_2824_0,           // 81
+    G_VER_1_0_2845_0,           // 82
+    G_VER_1_0_2944_0,           // 83
+    G_VER_1_0_3028_0,           // 84
+    G_VER_1_0_3095_0,           // 85
+    G_VER_1_0_3179_0,           // 86
+    G_VER_1_0_3258_0,           // 87
+    G_VER_1_0_3274_0,           // 88
+    G_VER_1_0_3323_0,           // 89
+    G_VER_1_0_3337_0,           // 90
+    G_VER_1_0_3351_0,           // 91
+    G_VER_1_0_3407_0,           // 92
+    G_VER_1_0_3411_0,           // 93
 };
+
+static std::vector<std::pair<SVersion, std::vector<int>>> ExeVersionMap = {
+    { {   0, 0 },   { -1} },
+    { { 335, 2 },   { G_VER_1_0_335_2_STEAM, G_VER_1_0_335_2_NOSTEAM } },
+    { { 350, 1 },   { G_VER_1_0_350_1_STEAM, G_VER_1_0_350_2_NOSTEAM } },
+    { { 372, 2 },   { G_VER_1_0_372_2_STEAM, G_VER_1_0_372_2_NOSTEAM } },
+    { { 393, 2 },   { G_VER_1_0_393_2_STEAM, G_VER_1_0_393_2_NOSTEAM } },
+    { { 393, 4 },   { G_VER_1_0_393_4_STEAM, G_VER_1_0_393_4_NOSTEAM } },
+    { { 463, 1 },   { G_VER_1_0_463_1_STEAM, G_VER_1_0_463_1_NOSTEAM } },
+    { { 505, 2 },   { G_VER_1_0_505_2_STEAM, G_VER_1_0_505_2_NOSTEAM } },
+    { { 573, 1 },   { G_VER_1_0_573_1_STEAM, G_VER_1_0_573_1_NOSTEAM } },
+    { { 617, 1 },   { G_VER_1_0_617_1_STEAM, G_VER_1_0_617_1_NOSTEAM } },
+    { { 678, 1 },   { G_VER_1_0_678_1_STEAM, G_VER_1_0_678_1_NOSTEAM } },
+    { { 757, 2 },   { G_VER_1_0_757_2_STEAM, G_VER_1_0_757_2_NOSTEAM } },
+    { { 757, 4 },   { G_VER_1_0_757_4_STEAM, G_VER_1_0_757_4_NOSTEAM } },
+    { { 791, 2 },   { G_VER_1_0_791_2_STEAM, G_VER_1_0_791_2_NOSTEAM } },
+    { { 877, 1 },   { G_VER_1_0_877_1_STEAM, G_VER_1_0_877_1_NOSTEAM } },
+    { { 944, 2 },   { G_VER_1_0_944_2_STEAM, G_VER_1_0_944_2_NOSTEAM } },
+    { { 1011, 1 },  { G_VER_1_0_1011_1_STEAM, G_VER_1_0_1011_1_NOSTEAM } },
+    { { 1032, 1 },  { G_VER_1_0_1032_1_STEAM, G_VER_1_0_1032_1_NOSTEAM } },
+    { { 1103, 2 },  { G_VER_1_0_1103_2_STEAM, G_VER_1_0_1103_2_NOSTEAM } },
+    { { 1180, 2 },  { G_VER_1_0_1180_2_STEAM, G_VER_1_0_1180_2_NOSTEAM } },
+    { { 1290, 1 },  { G_VER_1_0_1290_1_STEAM, G_VER_1_0_1290_1_NOSTEAM } },
+    { { 1365, 1 },  { G_VER_1_0_1365_1_STEAM, G_VER_1_0_1365_1_NOSTEAM } },
+    { { 1493, 0 },  { G_VER_1_0_1493_0_STEAM, G_VER_1_0_1493_0_NOSTEAM } },
+    { { 1493, 1 },  { G_VER_1_0_1493_1_STEAM, G_VER_1_0_1493_1_NOSTEAM } },
+    { { 1604, 0 },  { G_VER_1_0_1604_0_STEAM, G_VER_1_0_1604_0_NOSTEAM } },
+    { { 1604, 1 },  { G_VER_1_0_1604_1_STEAM, G_VER_1_0_1604_1_NOSTEAM } },
+  //{ { 1734, 0 },  { G_VER_1_0_1734_0_STEAM, G_VER_1_0_1734_0_NOSTEAM } },
+    { { 1737, 0 },  { G_VER_1_0_1737_0_STEAM, G_VER_1_0_1737_0_NOSTEAM } },
+    { { 1737, 6 },  { G_VER_1_0_1737_6_STEAM, G_VER_1_0_1737_6_NOSTEAM } },
+    { { 1868, 0 },  { G_VER_1_0_1868_0_STEAM, G_VER_1_0_1868_0_NOSTEAM } },
+    { { 1868, 1 },  { G_VER_1_0_1868_1_STEAM, G_VER_1_0_1868_1_NOSTEAM } },
+    { { 1868, 4 },  { G_VER_1_0_1868_4_EGS} },
+    { { 2060, 0 },  { G_VER_1_0_2060_0_STEAM, G_VER_1_0_2060_0_NOSTEAM} },
+    { { 2060, 1 },  { G_VER_1_0_2060_1_STEAM, G_VER_1_0_2060_1_NOSTEAM} },
+    { { 2189, 0 },  { G_VER_1_0_2189_0_STEAM, G_VER_1_0_2189_0_NOSTEAM} },
+    { { 2215, 0 },  { G_VER_1_0_2215_0_STEAM, G_VER_1_0_2215_0_NOSTEAM} },
+    { { 2245, 0 },  { G_VER_1_0_2245_0_STEAM, G_VER_1_0_2245_0_NOSTEAM} },
+    { { 2372, 0 },  { G_VER_1_0_2372_0_STEAM, G_VER_1_0_2372_0_NOSTEAM} },
+    { { 2545, 0 },  { G_VER_1_0_2545_0_STEAM, G_VER_1_0_2545_0_NOSTEAM} },
+    { { 2612, 1 },  { G_VER_1_0_2612_1_STEAM, G_VER_1_0_2612_1_NOSTEAM} },
+    { { 2628, 2 },  { G_VER_1_0_2628_2_STEAM, G_VER_1_0_2628_2_NOSTEAM} },
+    { { 2699, 0 },  { G_VER_1_0_2699_0_STEAM, G_VER_1_0_2699_0_NOSTEAM} },
+    { { 2699, 16 }, { G_VER_1_0_2699_16} },
+    { { 2802, 0 },  { G_VER_1_0_2802_0} },
+    { { 2824, 0 },  { G_VER_1_0_2824_0} },
+    { { 2845, 0 },  { G_VER_1_0_2845_0} },
+    { { 2944, 0 },  { G_VER_1_0_2944_0} },
+    { { 3028, 0 },  { G_VER_1_0_3028_0} },
+    { { 3095, 0 },  { G_VER_1_0_3095_0} },
+    { { 3179, 0 },  { G_VER_1_0_3179_0} },
+    { { 3258, 0 },  { G_VER_1_0_3258_0} },
+    { { 3274, 0 },  { G_VER_1_0_3274_0} },
+    { { 3323, 0 },  { G_VER_1_0_3323_0} },
+    { { 3337, 0 },  { G_VER_1_0_3337_0} },
+    { { 3351, 0 },  { G_VER_1_0_3351_0} },
+    { { 3407, 0 },  { G_VER_1_0_3407_0} },
+    { { 3411, 0 },  { G_VER_1_0_3411_0} },
+};
+
+#define FX_ASI_BUILDS \
+    FX_ASI_BUILD 1011 BEGIN "\0" END \
+    FX_ASI_BUILD 1032 BEGIN "\0" END \
+    FX_ASI_BUILD 1103 BEGIN "\0" END \
+    FX_ASI_BUILD 1180 BEGIN "\0" END \
+    FX_ASI_BUILD 1290 BEGIN "\0" END \
+    FX_ASI_BUILD 1365 BEGIN "\0" END \
+    FX_ASI_BUILD 1493 BEGIN "\0" END \
+    FX_ASI_BUILD 1604 BEGIN "\0" END \
+    FX_ASI_BUILD 1734 BEGIN "\0" END \
+    FX_ASI_BUILD 1737 BEGIN "\0" END \
+    FX_ASI_BUILD 1868 BEGIN "\0" END \
+    FX_ASI_BUILD 2060 BEGIN "\0" END \
+    FX_ASI_BUILD 2189 BEGIN "\0" END \
+    FX_ASI_BUILD 2215 BEGIN "\0" END \
+    FX_ASI_BUILD 2245 BEGIN "\0" END \
+    FX_ASI_BUILD 2372 BEGIN "\0" END \
+    FX_ASI_BUILD 2545 BEGIN "\0" END \
+    FX_ASI_BUILD 2612 BEGIN "\0" END \
+    FX_ASI_BUILD 2628 BEGIN "\0" END \
+    FX_ASI_BUILD 2699 BEGIN "\0" END \
+    FX_ASI_BUILD 2802 BEGIN "\0" END \
+    FX_ASI_BUILD 2824 BEGIN "\0" END \
+    FX_ASI_BUILD 2845 BEGIN "\0" END \
+    FX_ASI_BUILD 2944 BEGIN "\0" END \
+    FX_ASI_BUILD 3028 BEGIN "\0" END \
+    FX_ASI_BUILD 3095 BEGIN "\0" END \
+    FX_ASI_BUILD 3179 BEGIN "\0" END \
+    FX_ASI_BUILD 3258 BEGIN "\0" END \
+    FX_ASI_BUILD 3274 BEGIN "\0" END \
+    FX_ASI_BUILD 3323 BEGIN "\0" END \
+    FX_ASI_BUILD 3337 BEGIN "\0" END \
+    FX_ASI_BUILD 3351 BEGIN "\0" END \
+    FX_ASI_BUILD 3407 BEGIN "\0" END \
+    FX_ASI_BUILD 3411 BEGIN "\0" END
 
 static std::string eGameVersionToString(int version) {
     if (version > GameVersionString.size() - 1 || version < 0) {
         return std::to_string(version);
     }
     return GameVersionString[version];
+}
+
+template < typename K, typename V >
+V findNextLowest(const std::vector<std::pair<K, V>>& map, const K& key) {
+    for (auto it = map.rbegin(); it != map.rend(); ++it) {
+        if (it->first <= key)
+            return it->second;
+    }
+    return map.begin()->second;
 }
